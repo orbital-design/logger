e450591348085f3070b88c86df3c2006a10593e4
addzycullen <adam.cullen@live.co.uk>
2020-02-12 16:28:31 +0000

    Added conditionals & files for Kirki & Aqua Resizer, removed unneeded code and commented out code for refactoring

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
  * @author    Adam Cullen <adam.cullen@live.co.uk>
  * @copyright 2020 Adam Cullen
  * @license   https://www.gnu.org/licenses/gpl-2.0.html GPL-2.0-or-later
- * @link      https://github.com/addzycullen/sophia-master
+ * @link      https://github.com/addzycullen/logger
  */
 
 define( 'LOGGER_VERSION', '1.0.0' );
@@ -22,26 +22,6 @@ define( 'LOGGER_MINIMUM_PHP_VERSION', '7.2' );
 if ( version_compare( $GLOBALS['wp_version'], SOPHIA_MINIMUM_WP_VERSION, '<' ) || version_compare( phpversion(), SOPHIA_MINIMUM_PHP_VERSION, '<' ) ) {
     require_once get_template_directory() . 'inc/back-compat.php';
     return;
-}
-
-/**
- * Timber starter-theme
- * https://github.com/timber/starter-theme
- *
- * @package  WordPress
- * @subpackage  Timber
- * @since   Timber 0.1
- */
-
-/**
- * If you are installing Timber as a Composer dependency in your theme, you'll need this block
- * to load your dependencies and initialize Timber. If you are using Timber via the WordPress.org
- * plug-in, you can safely delete this block.
- */
-$composer_autoload = __DIR__ . '/vendor/autoload.php';
-if ( file_exists( $composer_autoload ) ) {
-	require_once $composer_autoload;
-	$timber = new Timber\Timber();
 }
 
 /**
@@ -60,11 +40,22 @@ if ( ! class_exists( 'Timber' ) ) {
 	add_filter(
 		'template_include',
 		function( $template ) {
-			return get_stylesheet_directory() . '/static/no-timber.html';
+			return get_stylesheet_directory() . '/no-timber.html';
 		}
 	);
 	return;
 }
+
+// Check if Kirki is loaded as a plugin, if not load the included version .
+if ( ! class_exists( 'Kirki' ) ) {
+    require_once get_template_directory() . '/inc/Kirki/kirki.php';
+}
+
+// Check if AQ_Resizer exists, if not load the included version.
+if ( ! class_exists( 'Aq_Resize' ) ) {
+    require_once get_template_directory() . '/inc/Aqua_Resizer/aq_resizer.php';
+}
+
 
 /**
  * Sets the directories (inside your theme) to find .twig files
@@ -78,115 +69,51 @@ Timber::$dirname = array( 'templates', 'views' );
 Timber::$autoescape = false;
 
 
+
 /**
  * We're going to configure our theme inside of a subclass of Timber\Site
  * You can move this to its own file and include here via php's include("MySite.php")
  */
-class StarterSite extends Timber\Site {
-	/** Add timber support. */
-	public function __construct() {
-		add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
-		add_filter( 'timber/context', array( $this, 'add_to_context' ) );
-		add_filter( 'timber/twig', array( $this, 'add_to_twig' ) );
-		add_action( 'init', array( $this, 'register_post_types' ) );
-		add_action( 'init', array( $this, 'register_taxonomies' ) );
-		parent::__construct();
-	}
-	/** This is where you can register custom post types. */
-	public function register_post_types() {
+// class StarterSite extends Timber\Site {
+// 	/** Add timber support. */
+// 	public function __construct() {
+// 		add_filter( 'timber/context', array( $this, 'add_to_context' ) );
+//         add_filter( 'timber/twig', array( $this, 'add_to_twig' ) );
 
-	}
-	/** This is where you can register custom taxonomies. */
-	public function register_taxonomies() {
+// 		parent::__construct();
+//     }
+// 	/** This is where you add some context
+// 	 *
+// 	 * @param string $context context['this'] Being the Twig's {{ this }}.
+// 	 */
+// 	public function add_to_context( $context ) {
+// 		$context['foo']   = 'bar';
+// 		$context['stuff'] = 'I am a value set in your functions.php file';
+// 		$context['notes'] = 'These values are available everytime you call Timber::context();';
+// 		$context['menu']  = new Timber\Menu();
+// 		$context['site']  = $this;
+// 		return $context;
+// 	}
 
-	}
+// 	/** This Would return 'foo bar!'.
+// 	 *
+// 	 * @param string $text being 'foo', then returned 'foo bar!'.
+// 	 */
+// 	public function myfoo( $text ) {
+// 		$text .= ' bar!';
+// 		return $text;
+// 	}
 
-	/** This is where you add some context
-	 *
-	 * @param string $context context['this'] Being the Twig's {{ this }}.
-	 */
-	public function add_to_context( $context ) {
-		$context['foo']   = 'bar';
-		$context['stuff'] = 'I am a value set in your functions.php file';
-		$context['notes'] = 'These values are available everytime you call Timber::context();';
-		$context['menu']  = new Timber\Menu();
-		$context['site']  = $this;
-		return $context;
-	}
+// 	/** This is where you can add your own functions to twig.
+// 	 *
+// 	 * @param string $twig get extension.
+// 	 */
+// 	public function add_to_twig( $twig ) {
+// 		$twig->addExtension( new Twig\Extension\StringLoaderExtension() );
+// 		$twig->addFilter( new Twig\TwigFilter( 'myfoo', array( $this, 'myfoo' ) ) );
+// 		return $twig;
+// 	}
 
-	public function theme_supports() {
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+// }
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
-
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support(
-			'html5',
-			array(
-				'comment-form',
-				'comment-list',
-				'gallery',
-				'caption',
-			)
-		);
-
-		/*
-		 * Enable support for Post Formats.
-		 *
-		 * See: https://codex.wordpress.org/Post_Formats
-		 */
-		add_theme_support(
-			'post-formats',
-			array(
-				'aside',
-				'image',
-				'video',
-				'quote',
-				'link',
-				'gallery',
-				'audio',
-			)
-		);
-
-		add_theme_support( 'menus' );
-	}
-
-	/** This Would return 'foo bar!'.
-	 *
-	 * @param string $text being 'foo', then returned 'foo bar!'.
-	 */
-	public function myfoo( $text ) {
-		$text .= ' bar!';
-		return $text;
-	}
-
-	/** This is where you can add your own functions to twig.
-	 *
-	 * @param string $twig get extension.
-	 */
-	public function add_to_twig( $twig ) {
-		$twig->addExtension( new Twig\Extension\StringLoaderExtension() );
-		$twig->addFilter( new Twig\TwigFilter( 'myfoo', array( $this, 'myfoo' ) ) );
-		return $twig;
-	}
-
-}
-
-new StarterSite();
+// new StarterSite();
