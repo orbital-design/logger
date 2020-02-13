b950fbd11c6f8d5e6c02dc99169a157ad6817261
addzycullen <adam.cullen@live.co.uk>
2020-02-13 16:32:13 +0000

    Added SVG Support to PHP Includes

---
+++ inc/Mime_Types/Component.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Logger\Mime_Types\Component class
+ *
+ * @since 1.0.0
+ *
+ * @package Logger
+ */
+
+namespace Logger\Mime_Types;
+
+use Logger\Component_Interface;
+use function add_filter;
+
+/**
+ * Class for adding basic theme support for mime types.
+ */
+class Component implements Component_Interface {
+
+    /**
+     * Gets the unique identifier for the theme component.
+     *
+     * @return string Component slug.
+     */
+    public function getSlug() : string
+    {
+        return 'mime_types';
+    }
+
+    /**
+     * Adds the action and filter hooks to integrate with WordPress.
+     */
+    public function initialize()
+    {
+        add_filter( 'upload_mimes', [ $this, 'addSvgSupport' ] );
+    }
+
+    /**
+     * SVG Support
+     *
+     * Adds SVG to the mime types supported (useful for gallery uploads in the WP Backend).
+     */
+    public function addSvgSupport($mimes)
+    {
+        $mimes['svg'] = 'image/svg+xml';
+        return $mimes;
+    }
+}
